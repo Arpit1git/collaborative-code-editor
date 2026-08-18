bd5d5880aa7b9f2d42ab6b86d7070b779971c7b1
952arpit16 <arpit@gopro.com>
2026-08-19 00:22:56 +0530

    feat: setup for ioredis

---
+++ Backend/src/Queue/connection_queue.js
@@ -0,0 +1,10 @@
+import IORedis from 'ioredis';
+
+export const redisConnection = new IORedis({
+    host: '127.0.0.1',
+    port: 6379,
+    maxRetriesPerRequest: null
+});
+
+redisConnection.on('connect', () => console.log('Redis connected for BullMQ!'));
+redisConnection.on('error', (err) => console.error('Redis connection error:', err))
